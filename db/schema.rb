1129bde304c8bf910a12c25aa5adf760340af9d9
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-10-14 13:14:51 +0100

    Update users table with avatar, cover_image, and full_name fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_114916) do
+ActiveRecord::Schema.define(version: 2020_10_14_120711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 2020_10_13_114916) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "full_name"
+    t.string "avatar"
+    t.string "cover_image"
   end
 
 end
