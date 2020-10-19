b607a0e51ddc641051ccca3b8fffc7a32e239633
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-10-19 03:59:58 +0100

    Add associations for comments and set up migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_121601) do
+ActiveRecord::Schema.define(version: 2020_10_19_024718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_10_14_121601) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "feeling_id", null: false
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["feeling_id"], name: "index_comments_on_feeling_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "feelings", force: :cascade do |t|
@@ -61,4 +71,6 @@ ActiveRecord::Schema.define(version: 2020_10_14_121601) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "feelings"
+  add_foreign_key "comments", "users"
 end
