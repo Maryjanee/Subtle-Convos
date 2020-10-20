64d12313036d9e85717ec8853068a9de0afb2fda
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-10-20 06:32:41 +0100

    Add tests for Relationship Model

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    it { should belong_to(:follower).class_name('User') }
+    it { should belong_to(:followed).class_name('User') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:follower_id) }
+    it { should validate_presence_of(:followed_id) }
+
+end
+
 end
