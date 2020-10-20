f9bfb96f3e405d28f79afc592e982f7dce143a94
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-10-20 06:24:52 +0100

    Add tests for Feeling model

--- spec/models/feeling_spec.rb
+++ spec/models/feeling_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Feeling, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Association' do
+    it { should belong_to(:user) }
+  end
+  
+  describe 'Validations' do
+    it { should validate_presence_of(:body) }
+  end
+  
 end
